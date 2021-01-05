77f9e6029cff46a33503ccf22b0ba308d1e51ffd
Sarp Uner <sarpim@gmail.com>
2021-01-04 19:34:27 -0500

    cha2eaf fully converts all tiers now.

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 ### Scripts for cha <----> eaf and opf <----> eaf conversions.
 
+#### data/
+
+This directory contains all the data used for testing purposes.
+
 #### awker.sh
 
 A shell script that is mainly a wrapper around an awk script. It extracts annotations from the ELAN csv output to make it look like the output of parse_clan2.py. 
 
+#### cha2eaf.py
+
+Old script from previous work that was used to convert cha files to eaf. Might prove useful.
 
 #### use_pmypi.py
 
 A python script that will convert a cha file to an eaf. It's requirements are in the requirements.txt file. First, install the dependencies with ```pip install -r requirements.txt```
+
+#### grep_sed_commands.txt
+
+Short commands that I have found useful are saved here.
 
 
--- cha2eaf.py
+++ cha2eaf.py
@@ -6,31 +6,50 @@ from os import path
 import pympi as pym
 import pyclan
 
+import pdb
+
 # Just set the default signal to be able to pipe the output.
 signal(SIGPIPE, SIG_DFL)
 
 # Participant dictionary, may become useful later, but currently not used.
 P_DICT = {}
 
-# Properties dictionary, used to check uniqueness of property keys. This is not mandatory, but the forums suggest this: https://tla.mpi.nl/topic/embed-metadata-in/
+# Properties dictionary, used to check uniqueness of property keys. This is
+# not mandatory, but the forums suggest this: https://tla.mpi.nl/topic/embed-metadata-in/
 PROP_DICT = {}
 
 # Dictionaries for LENA conversation and pause header information.
 CONV_DICT = {}
 PAUS_DICT = {}
 
-TEMP_LIST = [] 
-# Some string constants from CHAT files for ease of use. 
+TEMP_LIST = []
+# Some string constants from CHAT files for ease of use.
 PARTICIPANTS = '@Participants:'
 BG = '@Bg:'
 EG = '@Eg:'
-   
+
+# Other helpful constants, some defined by EAF specification, some by me. 
+SYMAC = 'Symbolic_Association'
+# linguistic type name for dependent tiers. I just picked this name, unlike
+# the constant above, which is a definition necessary for pympi.
+DEPENDENT = 'dependent' 
+
+COM = '%com'
+XCOM = '%xcom'
+XDB = '%xdb'
+BLOCK = 'block'
+
+
 def set_args():
     """Sets command line arguments. """
 
     parser = argparse.ArgumentParser(description="Convert CHA (CLAN) file to EAF (ELAN) file")
     parser.add_argument('cha_file', help='The CHA file to be converted')
-    parser.add_argument('-o', '--output', help='The output EAF file name. Optional, default is the input file name.', default='')
+    parser.add_argument(
+        '-o', '--output',
+        help='The output EAF file name. Optional, default is the input file name.',
+        default=''
+    )
     return parser.parse_args()
 
 def process_line(line, eaf):
@@ -44,11 +63,9 @@ def process_line(line, eaf):
 
     # Case where the line is a header line.
     elif line.startswith('@'):
-        stdout.write('Processing header line\n')
 
         # Process participants header. 
         if line.startswith(PARTICIPANTS):
-            stdout.write('Processing Participants header\n')
 
             # A list of participants in the form: ['SIL Silence LENA', ' MAN Male_Adult_Near Male', ...] 
             participants = line.replace(PARTICIPANTS, '').strip().split(',')
@@ -56,8 +73,6 @@ def process_line(line, eaf):
             # Key value pair for the eaf property header element. 
             key = PARTICIPANTS
             value = '\n'.join([i.strip() for i in participants])
-
-            stdout.write(value + '\n')
 
             # Adding the participants to the eaf property element
             if key not in prop_dict:
@@ -70,7 +85,7 @@ def process_line(line, eaf):
             return
 
     # Line is a main tier. 
-    elif line.startswith(MAIN):
+    elif line.startswith("MAIN"):
         llist = line.strip().split()
 
 def process_buf(llist):
@@ -83,21 +98,45 @@ def process_buf(llist):
     elif len(llist) == 1:
         return llist[0]
 
-    # Format the possible multiline! 
+    # Format the possible multiline!
     else:
-        stderr.write('Found a multiline!')
+        stderr.write('Found a multiline!\n')
         new_line = ' '.join(llist)
-        stderr.write(new_line)
+        stderr.write(new_line + '\n')
         return new_line
 
-
-def cha2eaf(inpf, outf):
-    """Converts a given cha file to an eaf file. """
-
+def setup_eaf():
+    """Initial setup of the eaf filne is handled here to reduce complexity."""
     eaf = pym.Elan.Eaf()
-    cha = pyclan.ClanFile(inpf)
-    participants = {}
+
+    eaf.add_linguistic_type(
+            XDB, 
+            constraints=SYMAC, 
+            timealignable=False
+            )
+
+    eaf.add_linguistic_type(
+            COM, 
+            constraints=SYMAC, 
+            timealignable=False
+            )
+
+    eaf.add_linguistic_type(
+            XCOM, 
+            constraints=SYMAC, 
+            timealignable=False
+            )
+
+    eaf.add_tier(
+            BLOCK
+            )
+    return eaf
+
+def _process_header(cha, eaf):
+
     buf = []
+
+    # Processing the header. TODO: Extract to a function
     for linum, line in enumerate(cha.get_header()):
         # If line is either a header, main tier, or dependent tier:
         if line.line.startswith(('@', '*', '%')):
@@ -108,16 +147,76 @@ def cha2eaf(inpf, outf):
         # If line is none of the above, we assume that it is a continuation!
         buf.append(line.line.strip())
 
+def add_ref_annotation(line, eaf):
+    # lines of the form '%com:\t|BC|1|0|0|XIOCA|NT|FI|\n'
+    # Doing these modifications here so as to not break pyclan elsewhere
+    # in case that breaks something.
+
+    name = line.tier + '@' + line.parent_tier
+    if name not in eaf.get_tier_names():
+        eaf.add_tier(
+                tier_id = name,
+                ling = line.tier,
+                parent = line.parent_tier
+                )
+
+    eaf.add_ref_annotation(
+            name,
+            line.parent_tier,
+            (line.onset + line.offset)/2,
+            line.content)
+
+def cha2eaf(inpf, outf):
+    """Converts a given cha file to an eaf file. """
+
+    eaf = setup_eaf()
+    cha = pyclan.ClanFile(inpf)
+    participants = {}
+
+    # Currently not returning anything, but might return a participant database in the future?
+    _process_header(cha, eaf)
+
     # Processing the rest of the file after the header.
     end = cha.get_header()[-1].index
-    for line in cha.line_map[end:]:
+    # The plus one below starts the iteration right after the last header line.
+    for line in cha.line_map[end+1:]:
         # This is a tier line.
         if line.is_tier_line:
             # If the tier does not exist, add it first. 
             if line.tier not in eaf.get_tier_names():
                 eaf.add_tier(line.tier)
 
-            eaf.add_annotation(line.tier, line.onset, line.offset, line.content)
+        elif line.is_paus_block_delimiter or line.is_conv_block_delimiter:
+            line.tier = BLOCK
+            line.content = line.line.strip()
+
+            # pympi does not like onset and offset being the same...
+            if line.onset == 0 and line.offset == 0:
+                line.onset, line.offset = 0, 1
+    
+        elif line.is_clan_comment:
+            line.parent_tier = line.tier
+            line.tier, line.content = line.line.strip().split('\t')
+            add_ref_annotation(line, eaf)
+            continue
+
+        elif line.xdb_line:
+            line.tier = XDB
+            add_ref_annotation(line, eaf)
+            continue
+
+        elif line.is_end_header:
+            # This is just the end header. It is not necessary to add it.
+            continue
+
+        else:
+            print(line)
+            print('UNPROCESSED LINE ABOVE')
+            continue
+
+
+        last_annotation = (line.tier, line.onset, line.offset, line.content)
+        eaf.add_annotation(*last_annotation)
 
     # If no output file option is supplied. 
     if outf == '':
